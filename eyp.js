7d547430486e5a5b9db71a5d74dfdf8f5bb4bde8
eyupokk <bblnt12345@gmail.com>
2024-01-31 16:08:47 +0300

    sss
    sss

--- eyp.js
+++ eyp.js
@@ -1,4 +1,10 @@
-function(setTrayOnMinimize)setTrayOnMinimize()
+(setTrayOnMinimize) ();
      createTrayNotification( "Dikkat:Oyunu Alt Tab'a Attınız", "warning" )
 end
 addEventHandler( "onClientMinimize", getRootElement( ), setTrayOnMinimize ) 
+ 
+ { addfunction("dikkat oyunu alt taba attınız!!?!?")
+
+  console.warn("alt tab atmaktan çekininiz!");
+ }
+                  
